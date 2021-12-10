f3f87acf45b2cd5878a26e154bd2c091427bbbb8
Dima Lol <zonex1995@gmail.com>
2021-12-10 16:14:01 +0200

    added validation for register user

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -6,7 +6,8 @@ from rest_framework.serializers import (
     HyperlinkedIdentityField,
     ModelSerializer,
     SerializerMethodField,
-    ValidationError
+    ValidationError,
+    EmailField
 )
 
 User = get_user_model()
@@ -29,12 +30,42 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserCreateSerializer(ModelSerializer):
+    email = EmailField(label='Email Address')
+    email2 = EmailField(label='Confirm Email')
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'email2', 'password']
         extra_kwargs = {'password':
                             {'write_only': True}
                         }
+
+    def validate(self, data):
+        # email = data['email']
+        # user_qs = User.objects.filter(email=email)
+        # if user_qs.exists():
+        #     raise ValidationError("This user has already registered")
+        return data
+
+    def validate_email(self, value):
+        data = self.get_initial()
+        email1 = data.get("email2")
+        email2 = value
+        if email1 != email2:
+            raise ValidationError("Emails must match.")
+
+        user_qs = User.objects.filter(email=email2)
+        if user_qs.exists():
+            raise ValidationError("This user has already registered")
+        return value
+
+    def validate_email2(self, value):
+        data = self.get_initial()
+        email1 = data.get("email")
+        email2 = value
+        if email1 != email2:
+            raise ValidationError("Emails must match.")
+        return value
 
     def create(self, validated_data):
         username = validated_data['username']
@@ -44,6 +75,6 @@ class UserCreateSerializer(ModelSerializer):
             username=username,
             email=email
         )
-        user_obj.set_password(password) # stoped on 27 video
+        user_obj.set_password(password)
         user_obj.save()
         return validated_data
